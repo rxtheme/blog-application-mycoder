6faa31e37d231a5c792f00bc3d634d2daaa39936
yousufislamme <online.okk@gmail.com>
2023-07-28 10:52:49 +0600

    install Prettier

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -1,20 +1,15 @@
-import Header from "@/components/Header/Header";
+import Header from '@/components/Header/Header';
 
 const Hero = () => {
-   return (
-      <div>
-         <Header />
-            <div className="flex">
-         <div className="m-2 rounded-lg w-[60%] bg-gray-300 h-[500px]">
-            </div>
-               <div className="bg-pink-200 w-[35%] h-[200px]">
-                  
-               </div>
-            
-</div>
-
+  return (
+    <div>
+      <Header />
+      <div className="flex">
+        <div className="m-2 rounded-lg w-[60%] bg-gray-300 h-[500px]"></div>
+        <div className="bg-pink-200 w-[35%] h-[200px]"></div>
       </div>
-   );
+    </div>
+  );
 };
 
 export default Hero;
