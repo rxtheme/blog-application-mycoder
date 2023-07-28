1e0cadafda4dd78c8d69f5197eec08b16f436898
yousufislamme <online.okk@gmail.com>
2023-07-28 11:23:33 +0600

    just update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,49 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Header = () => {
-   const router = useRouter();
+  const router = useRouter();
   return (
-     <div>
-        <div className="flex justify-between px-10 bg-gray-300 py-3 rounded-xl w-[98%] mx-auto mt-2 border-2 border-purple-500">
-         <div><Link className="font-semibold" href='/'>Home</Link></div>
-           <div>
-              <ul className="flex gap-3"> 
-                 <li className="h-full"><Link className={router.pathname == '/blogs' ? 'font-semibold' : ''}
-                    href='/blogs'
-                 >Blog</Link></li>
-
-                 <li className="h-full"><Link className={router.pathname == '/about' ? 'font-semibold' : ''} href='/about'>About</Link></li> 
-                 
-                 <li className="h-full"><Link className={router.pathname == '/contact' ? 'font-semibold': ''} href='/contact'>Contact</Link></li> 
-              </ul>
-           </div>
-           
+    <div>
+      <div className="flex justify-between px-10 bg-gray-300 py-3 rounded-xl w-[98%] mx-auto mt-2 border-2 border-purple-500">
+        <div>
+          <Link className="font-semibold" href="/">
+            Home
+          </Link>
         </div>
-    </div>
-  )
-}
+        <div>
+          <ul className="flex gap-3">
+            <li className="h-full">
+              <Link
+                className={router.pathname == '/blogs' ? 'font-semibold' : ''}
+                href="/blogs"
+              >
+                Blog
+              </Link>
+            </li>
 
-export default Header
+            <li className="h-full">
+              <Link
+                className={router.pathname == '/about' ? 'font-semibold' : ''}
+                href="/about"
+              >
+                About
+              </Link>
+            </li>
+
+            <li className="h-full">
+              <Link
+                className={router.pathname == '/contact' ? 'font-semibold' : ''}
+                href="/contact"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -1,14 +1,22 @@
+import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
 
 const Hero = () => {
   return (
-    <div>
-      <Header />
-      <div className="flex">
-        <div className="m-2 rounded-lg w-[60%] bg-gray-300 h-[500px]"></div>
-        <div className="bg-pink-200 w-[35%] h-[200px]"></div>
+    <>
+      <div className="container mx-auto px-5">
+        <Header />
+        <div className="grid grid-cols-4 gap-5 my-3">
+          <div className="col-span-3 rounded-lg bg-gray-300 h-[450px]"></div>
+
+          <div className="col-span-1 rounded-lg space-y-5">
+            <div className="col-span-1 rounded-lg bg-blue-300 h-[215px]"></div>
+            <div className="col-span-1 rounded-lg bg-pink-300 h-[215px]"></div>
+          </div>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
