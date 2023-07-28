e83bd1975fd749012860e53a1cdd34057ff672ed
yousufislamme <online.okk@gmail.com>
2023-07-28 12:48:26 +0600

    create a BlogLayout stacture design

---
+++ src/components/BlogLayout/BlogLayout.jsx
@@ -0,0 +1,33 @@
+import Heading from '../Heading/Heading';
+
+const BlogLayout = () => {
+  return (
+    <>
+      <div className="bg-green-100">
+        <Heading
+          title="Blog List"
+          class1="text-3xl text-slate-800 font-bold"
+          class2="text-slate-600"
+          desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, eius?"
+        />
+
+        {/* Blog list layout design */}
+        <div className="mx-10">
+          <div className="flex flex-col gap-5">
+            <div className="bg-blue-400 w-full h-52 rounded-lg shadow-xl">
+              <div className="h-full w-[300px] bg-slate-700 rounded-lg"></div>
+            </div>
+            <div className="flex flex-row-reverse bg-blue-400 w-full h-52 rounded-lg shadow-xl">
+              <div className="h-full w-[300px] bg-slate-700 rounded-lg"></div>
+            </div>
+            <div className="bg-blue-400 w-full h-52 rounded-lg shadow-xl">
+              <div className="h-full w-[300px] bg-slate-700 rounded-lg"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BlogLayout;
