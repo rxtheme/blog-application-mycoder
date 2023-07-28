936836b0bfea85697c50e8763eb8021e5db741cd
yousufislamme <online.okk@gmail.com>
2023-07-28 11:23:00 +0600

    first Footer create

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div>
+      <div className="container mx-auto px-5 h-10 bg-slate-800 flex justify-center items-center">
+        <p className="text-slate-200">
+          © 2023-2024 [MyCoderBlog]. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
